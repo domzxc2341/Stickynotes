12562ddc1acfecb97627d567a049998f95dc2820
domzxc2341 <80136346+domzxc2341@users.noreply.github.com>
2022-07-11 14:35:32 -0800

    ye
    ye

---
+++ stickynotes/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl text-center relative top-20">Hays hays</h1>
+      </div>
+  );
+}
+
+export default App;
